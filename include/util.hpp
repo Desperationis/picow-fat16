db7a840a4301d87eebb070dc2a4369067e0481a6
Desperationis <smarttdiego@gmail.com>
2023-07-22 00:24:28 -0700

    Quality of Life improvements
    * Organize code into include/ and src/
    * Add safe_printf
        * Tiny USB takes the entire USB channel, so UART has to be used. Due
          to thin wires and BAUD issues, I wrote a custom printf that
          outputs to the screen slow enough that text corruption is minimal.
    * stateless_led
        * This lets you know if the pico is on and working
    * Scripts
        * compile.bash
    	* Cleans project files then re-builds everything.
        * install.bash
    	* Install .uf2 to pi mounted on /media

---
+++ include/util.hpp
@@ -0,0 +1,52 @@
+#include "device/usbd.h"
+#include <stdarg.h>
+#include "string.h"
+#include "pico/cyw43_arch.h"
+#include "bsp/board.h"
+
+#define UART_ID uart0
+#define BAUD_RATE 115200
+
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+
+/**
+ * Slowly print out string to UART to reduce impedeance on thin wires.
+ */
+void safe_print(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    // Calculate the size of the buffer needed
+    int size = vsnprintf(nullptr, 0, format, args);
+
+    // Allocate memory for the buffer
+    char* buffer = new char[size + 1];
+
+    // Fill the buffer with the formatted string
+    vsnprintf(buffer, size + 1, format, args);
+
+    va_end(args);
+
+	for (int i = 0;  i < strlen(buffer); i++) {
+		uart_putc(UART_ID, buffer[i]);
+		sleep_ms(10);
+	}
+
+    delete[] buffer;
+}
+
+
+void stateless_led_blink() {
+	static uint32_t previous_ms = 0;
+	static bool led_toggle = false;
+
+	if ( board_millis() - previous_ms < 1000 )
+		return;
+
+	led_toggle = !led_toggle;
+	previous_ms = board_millis();
+
+	cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1 ? led_toggle : 0);
+}
---
+++ src/main.cpp
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <hardware/flash.h>
+#include "device/usbd.h"
+#include "tusb.h"
+#include "pico.h"
+#include "pico/stdlib.h"
+#include "bsp/board.h"
+#include "pico/cyw43_arch.h"
+#include "hardware/uart.h"
+#include "util.hpp"
+
+int main() {
+    // Initialise UART
+	uart_init(UART_ID, BAUD_RATE);
+	gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+    // Initialize USB Pins and Protocol. This overrides USB communication, so
+    // UART must be used.
+    board_init();
+    tusb_init();
+
+    if (cyw43_arch_init()) {
+        safe_print("Wi-Fi init failed");
+        return -1;
+    }
+
+	safe_print("--------SYSTEM START--------\n");
+	safe_print("Total flash size is: %d\n", PICO_FLASH_SIZE_BYTES);
+	safe_print("Sector Size %d\n", FLASH_SECTOR_SIZE);
+	safe_print("Block Size %d\n", FLASH_BLOCK_SIZE);
+	safe_print("Page Size %d\n", FLASH_PAGE_SIZE);
+
+
+	
+    /*while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
+    }*/
+
+	while (1) {
+		tud_task();
+		stateless_led_blink();
+	}
+}
+
+
+
+//--------------------------------------------------------------------+
+// Device callbacks
+//--------------------------------------------------------------------+
+
+// Invoked when device is mounted
+void tud_mount_cb(void)
+{
+}
+
+// Invoked when device is unmounted
+void tud_umount_cb(void)
+{
+}
+
+// Invoked when usb bus is suspended
+// remote_wakeup_en : if host allow us  to perform remote wakeup
+// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+void tud_suspend_cb(bool remote_wakeup_en)
+{
+  (void) remote_wakeup_en;
+}
+
+// Invoked when usb bus is resumed
+void tud_resume_cb(void)
+{
+}
+
